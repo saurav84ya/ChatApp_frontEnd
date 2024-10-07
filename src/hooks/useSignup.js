1fc43f60e3d80ba77b388fe87fe378f5a619c7b0
Saurav Chaurasia <155281737+saurav84ya@users.noreply.github.com>
2024-10-07 09:18:08 +0530

    Update useSignup.js

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -48,7 +48,7 @@ const useSignup = () => {
     try {
       setLoading(true)
 
-      const res = await fetch(`${import.meta.env.VITE_SERVER}/api/auth/signup`, {
+      const res = await fetch(`https://chatapp-backend-lbyl.onrender.com/api/auth/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
