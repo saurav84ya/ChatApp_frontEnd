96a5ca0f7b8c8a091e666327065526823f994668
Saurav Chaurasia <155281737+saurav84ya@users.noreply.github.com>
2024-11-15 23:27:49 +0530

    Update MessageInput.jsx

--- src/component/messages/MessageInput.jsx
+++ src/component/messages/MessageInput.jsx
@@ -18,19 +18,19 @@ const MessageInput = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="p-4 mb-8"
+      className="p-4 mb-[30px] md:mb-2 "
     >
-      <div className="relative bg-red-400 p-2 w-full">
+      <div className="relative  p-2 w-full">
         <input
           type="text"
           placeholder="Enter your text..."
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-          className="border border-gray-600 bg-gray-700 text-white text-sm rounded-lg px-3 py-2 w-full focus:outline-none focus:border-blue-500"
+          className="border border-gray-600 bg-gray-700 text-white text-sm rounded-lg px-3 py-2 h-[70px] sm:h-[auto] w-full focus:outline-none focus:border-blue-500"
         />
         <button
           type="submit"
-          className="absolute top-1/2 right-3 transform -translate-y-1/2 flex items-center text-white hover:text-blue-500"
+          className="absolute sm:top-1/2 sm:right-3 top-[35px] right-5 transform -translate-y-1/2 flex items-center text-white hover:text-blue-500"
           disabled={loading}
         >
           {loading ? <Spinner /> : <BsSend size={20} />}
